6eda392cfc263c7689e0d5f7558c031f4e771f4b
Akvalon42 <kostikshpitsa@mail.ru>
2025-11-09 13:07:22 +0300

    добавил класс Монет

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 
 import pygame as pg
 import pytmx
-import os
+
 
 pg.init()
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1020
 FPS = 80
-TILE_SCALE = 1.5
-win = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+TILE_SCALE = 2
+font = pg.font.Font(None, 36)
 
 class Platform (pg.sprite.Sprite):
     def __init__(self, image, x, y, width, height):
@@ -36,7 +36,7 @@ class Player(pg.sprite.Sprite):
         # Начальная скорость и гравитация
         self.velocity_x = 0
         self.velocity_y = 0
-        self.gravity = 0.01
+        self.gravity = 1
 
         self.is_jumping = False
 
@@ -49,11 +49,11 @@ class Player(pg.sprite.Sprite):
         self.hp = 10
         self.damage_timer = pg.time.get_ticks()
         self.damage_interval = 1000
-        self.hitbox = [self.rect.x, self.rect.y, 18 * 3, 18 * 5 ]
+        self.hitbox = pg.Rect(self.rect.x, self.rect.y, 16 * TILE_SCALE, 16 * TILE_SCALE * 1.5 + 8 )
 
     def load_animations(self):
         tile_size = 32
-        tile_scale = 9
+        tile_scale = TILE_SCALE * 3.5
 
         self.idle_animations_right = []
         num_images = 4
@@ -73,11 +73,12 @@ class Player(pg.sprite.Sprite):
 
         self.move_animations_left = [pg.transform.flip(image, True, False) for image in self.move_animations_right]
     def jump(self):
-        self.velocity_y = -12
+        self.velocity_y = -17
         self.is_jumping = True
     def update(self, platforms):
         keys = pg.key.get_pressed()
-        if keys[pg.K_SPACE] and not self.is_jumping:
+        # if keys[pg.K_SPACE]and not self.is_jumping:
+        if keys[pg.K_SPACE]:
             self.jump()
         if keys[pg.K_a]:
             if self.current_animation != self.move_animations_left:
@@ -98,33 +99,71 @@ class Player(pg.sprite.Sprite):
                 self.current_animation = self.move_animations_left
                 self.current_image = 0
             self.velocity_x = 0
-        new_x = self.rect.x + self.velocity_x
-        if 0 <= new_x <= self.map_width - self.rect.width:
-            self.rect.x = new_x
+        new_x = self.hitbox.x + self.velocity_x
+        if 0 <= new_x <= self.map_width - self.hitbox.width:
+            self.hitbox.x = new_x
         self.velocity_y += self.gravity
+        self.hitbox.y += self.velocity_y
 
 
         for platform in platforms:
-            if platform.rect.collidepoint((self.hitbox[0] + self.hitbox[2] // 2, self.hitbox[1] + self.hitbox[3])  ):
-                self.rect.bottom = platform.rect.top
+            if platform.rect.collidepoint(self.hitbox.midbottom):
+                self.hitbox.bottom = platform.rect.top
                 self.velocity_y = 0
                 self.is_jumping = False
-            if platform.rect.collidepoint((self.hitbox[0] + self.hitbox[2] // 2, self.hitbox[1] )):
-                self.rect.top = platform.rect.bottom
+                # print(platform)
+            if platform.rect.collidepoint(self.hitbox.midtop):
+                self.hitbox.top = platform.rect.bottom
                 self.velocity_y = 0
-            if platform.rect.collidepoint((self.hitbox[0] + self.hitbox[2], self.hitbox[1] + self.hitbox[3] // 2 )):
-                self.rect.right = platform.rect.left
-            if platform.rect.collidepoint((self.hitbox[0], self.hitbox[1] + self.hitbox[3] // 2 )):
-                self.rect.left = platform.rect.right
-        self.rect.y += self.velocity_y
-        self.hitbox[0] = self.rect.centerx - self.hitbox[2] // 2
-        self.hitbox[1] = self.rect.centery - self.hitbox[3] // 2
+                # print(platform)
+            if platform.rect.collidepoint(self.hitbox.midright):
+                self.hitbox.right = platform.rect.left
+                # print(platform)
+            if platform.rect.collidepoint(self.hitbox.midleft):
+                self.hitbox.left = platform.rect.right
+                # print(platform)
+
+        self.rect.center = (self.hitbox.centerx, self.hitbox.centery + 4)
+
         
         if pg.time.get_ticks() - self.timer > self.interval:
             self.current_image += 1
             if self.current_image >= len(self.current_animation):
                 self.current_image = 0
             self.image = self.current_animation[self.current_image]
+            self.timer = pg.time.get_ticks()
+class Coin(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Coin, self).__init__()
+        self.load_animations()
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(x=x*TILE_SCALE, y=y*TILE_SCALE)
+
+        self.current_image = 0
+        self.interval = 200
+        self.timer = pg.time.get_ticks()
+
+    def load_animations(self):
+        tile_size = 16
+        self.images= []
+
+        num_images = 5
+        spriteSheet = pg.image.load("maps/MonedaR.png")
+
+        for i in range(num_images):
+            x = i * tile_size
+            y = 0
+            rect = pg.Rect(x, y, tile_size, tile_size)
+            image = spriteSheet.subsurface(rect)
+            image = pg.transform.scale(image, (tile_size * TILE_SCALE, tile_size * TILE_SCALE))
+            self.images.append(image)
+
+    def update(self):
+        if pg.time.get_ticks() - self.timer > self.interval:
+            self.current_image += 1
+            if self.current_image >= len(self.images):
+                self.current_image = 0
+            self.image = self.images[self.current_image]
             self.timer = pg.time.get_ticks()
 class Game:
     def __init__(self):
@@ -137,16 +176,21 @@ class Game:
         self.background_1 = pg.transform.scale(self.background_1, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.background_2 = pg.image.load("./maps/free-swamp-game-tileset-pixel-art (1)/2 Background/Layers/5.png")
         self.background_2 = pg.transform.scale(self.background_2, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-        self.tmx_map = pytmx.load_pygame("maps/LVL_1.tmx")
+        self.one_coin = pg.image.load("maps/MonedaR.png")
+        rect = pg.Rect(0, 0,  16, 16)
+        image = self.one_coin.subsurface(rect)
+        self.one_coin = pg.transform.scale(image, (16 * TILE_SCALE, 16 * TILE_SCALE))
+        self.tmx_map = pytmx.load_pygame("maps/LVL_2.tmx")
         self.map_pixel_width = self.tmx_map.width * self.tmx_map.tilewidth * TILE_SCALE
         self.map_pixel_height = self.tmx_map.height * self.tmx_map.tileheight * TILE_SCALE
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
+        self.coins = pg.sprite.Group()
         self.player = Player(self.map_pixel_width, self.map_pixel_height)
         self.all_sprites.add(self.player)
+        self.collected_coins = 0
         for layer in self.tmx_map:
-            if layer.name == "болото":
+            if layer.name == "карта":
                 for x, y, gid in layer:
                     tile = self.tmx_map.get_tile_image_by_gid(gid)
 
@@ -156,6 +200,14 @@ class Game:
                                             self.tmx_map.tileheight)
                         self.all_sprites.add(platform)
                         self.platforms.add(platform)
+            elif layer.name == "монеты":
+                for x, y, gid in layer:
+                    tile = self.tmx_map.get_tile_image_by_gid(gid)
+
+                    if tile:
+                        coin = Coin( x * self.tmx_map.tilewidth, y * self.tmx_map.tileheight)
+                        self.all_sprites.add(coin)
+                        self.coins.add(coin)
         self.camera_x = 0
         self.camera_y = 0
         self.camera_speed = 2
@@ -171,10 +223,14 @@ class Game:
         quit()
     def update(self):
         self.player.update(self.platforms)
+        self.coins.update()
+        for coin in self.coins:
+            if coin.rect.colliderect(self.player.hitbox):
+                self.collected_coins += 1
+                coin.kill()
 
         self.camera_x = self.player.rect.x - SCREEN_WIDTH // 2
         self.camera_y = self.player.rect.y - SCREEN_HEIGHT // 2
-
         self.camera_x = max(0, min(self.camera_x, self.map_pixel_width - SCREEN_WIDTH))
         self.camera_y = max(0, min(self.camera_y, self.map_pixel_height - SCREEN_HEIGHT))
 
@@ -193,7 +249,14 @@ class Game:
             self.screen.blit(sprite.image, sprite.rect.move(-self.camera_x, -self.camera_y))
         self.player.hitbox[0] -= self.camera_x
         self.player.hitbox[1] -= self.camera_y
-        pg.draw.rect(win, "red", self.player.hitbox, 3)
+        # pg.draw.rect(self.screen, "red", self.player.hitbox, 3)
+        self.count_attribute_draw()
         pg.display.flip()
+    def count_attribute_draw(self):
+        self.screen.blit(self.one_coin, [10,10])
+        text = font.render(str(self.collected_coins), True, (255, 255, 255))
+        text_rect = text.get_rect(x=40, y=15)
+        self.screen.blit(text, text_rect)
+
 if __name__ == "__main__":
     game = Game()
